4fec2483270c73b20b072d65d99a82a92ee48d54
Alfonso1025 <alfonso25elorriaga@gmail.com>
2021-12-05 09:41:49 -0600

    first commit to the master branch

---
+++ src/routes/routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { connect } from '../twitter';
+
+const router = Router();
+
+router.get('/tweets/:id', async (req, res) => {
+    
+    //v2 Twitter
+    const tweets = await connect(req.params.id);
+    res.json(tweets.data.data[0].text);
+    console.log(req.params.id)
+    
+});
+
+router.get('/test/:id', (req, res)=>{
+
+    res.send(req.params.id)
+}
+    );
+
+export default router;
